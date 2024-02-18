3f03e4796e05df4fc57145ca0ddaff7a6fadf54e
Temirlan <temadjumanov@gmail.com>
2024-02-18 22:56:59 +0600

    Created edit task

--- routers/tasks.ts
+++ routers/tasks.ts
@@ -37,4 +37,37 @@ taskRouter.get("/", auth, async (req: RequestWithUser, res, next) => {
   }
 });
 
+taskRouter.put("/:id", auth, async (req: RequestWithUser, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).send({ error: "Task not found" });
+    }
+
+    if (task.user.toString() !== req.user?._id.toString()) {
+      return res
+        .status(403)
+        .send({ error: "You do not have permission to edit this task" });
+    }
+
+    if (req.body.title) {
+      task.title = req.body.title;
+    }
+    if (req.body.description) {
+      task.description = req.body.description;
+    }
+    if (req.body.status) {
+      task.status = req.body.status;
+    }
+    await task.save();
+
+    return res.send(task);
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(error);
+    }
+    next(error);
+  }
+});
+
 export default taskRouter;
