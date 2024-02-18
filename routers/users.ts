530474c3549b9115225e0da7799bf2521d42ac0e
Temirlan <temadjumanov@gmail.com>
2024-02-18 22:08:11 +0600

    created middleware, users router

---
+++ routers/users.ts
@@ -0,0 +1,50 @@
+import express from "express";
+import User from "../models/User";
+import mongoose from "mongoose";
+import auth, { RequestWithUser } from "../middleware/auth";
+
+const userRouter = express.Router();
+
+userRouter.post("/", async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    user.generateToken();
+    await user.save();
+    return res.send(user);
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(error);
+    }
+
+    next(error);
+  }
+});
+
+userRouter.post("/sessions", auth, async (req: RequestWithUser, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      return res.status(422).send({ error: "Username not found!" });
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+      return res.status(422).send({ error: "Password is wrong!" });
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({ message: "Username and password are correct!", user });
+  } catch (e) {
+    next(e);
+  }
+});
+
+export default userRouter;
